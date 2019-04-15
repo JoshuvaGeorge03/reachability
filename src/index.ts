1556db296f90f1785834d18eceee5a9d22e6d20b
joshuva george <joshuva@adaptivemedia.in>
2019-04-15 12:20:00 +0530

    implemented basic network reachability function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export const greet = () => {
-  return null;
-};
+export async function isReachable(url: string = 'https://www.google.com/') {
+  try {
+    const result = await fetch(url);
+    if (result.ok) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ src/reachability.test.ts
@@ -0,0 +1,5 @@
+import { isReachable } from './index';
+
+// test('is Reachable', () => {
+//     expect(isReachable('https://www.google.com/').toBe(boo))
+// });
